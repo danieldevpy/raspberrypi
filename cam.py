051cf69d1c624aee5e5aaad1779febca4fb0ca24
Daniel Fernandes <danielfernandes202@gmail.com>
2023-10-28 11:28:28 -0300

    shwo fps

--- cam.py
+++ cam.py
@@ -6,7 +6,9 @@ class CamModule:
   def __init__(self, port) -> None:
     self.cap = cv2.VideoCapture(port)
     self.last_frame = None
-
+    self.font = cv2.FONT_HERSHEY_SIMPLEX
+    self.prev_frame_time = 0
+    self.new_frame_time = 0
 
   def capture(self):
     ret, frame = self.cap.read()
@@ -15,6 +17,14 @@ class CamModule:
         print("Erro ao capturar o frame.")
         return
 
+
+    self.new_frame_time = time.time() 
+  
+    fps = 1/(self.new_frame_time-self.prev_frame_time) 
+    self.prev_frame_time = self.new_frame_time 
+  
+    fps = int(fps) 
+    cv2.putText(frame, str(fps), (7, 70), self.font, 3, (100, 255, 0), 3, cv2.LINE_AA) 
     self.last_frame = frame
 
   def run(self):
